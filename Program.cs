9d3017c30d81d5b29c2f9a3359c7b4163423e1f7
Adolfo Cantoral <pcantorall@miumg.edu.gt>
2023-06-17 08:52:42 -0600

    Serie3

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        int[,] matriz = new int[3, 2];
+        //Se asignan valores a la matriz
+        matriz[0, 0] = 1;
+        matriz[0, 1] = 2;
+        matriz[1, 0] = 3;
+        matriz[1, 1] = 4;
+        matriz[2, 1] = 5;
+        matriz[2, 2] = 6;        
+    }
+}
